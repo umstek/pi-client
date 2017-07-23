f20a9609a1dbe4e115a513c6be8391fc6c3a4b9a
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-07-23 14:10:37 +0530

    Router config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Button>Test</Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware,
+  push
+} from 'react-router-redux';
+
+import reducers from './reducers'; // Or wherever you keep your reducers
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+);
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <LocaleProvider locale={enUS}>
+        <App />
+      </LocaleProvider>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
